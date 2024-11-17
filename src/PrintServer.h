142abb986a01004aabaa1dd9f4c32cafe5111b30
sgrsn <sgrsn1711@gmail.com>
2024-11-17 23:14:04 +0900

    🚑 Add .h file

---
+++ src/PrintServer.h
@@ -0,0 +1,14 @@
+// ESP32Console
+// MIT License
+
+#pragma once
+
+#ifdef __cplusplus
+
+#include "print_server.hpp"
+
+#else
+
+#error ESP32Console requires a C++ compiler, please change file extension to .cc or .cpp
+
+#endif
